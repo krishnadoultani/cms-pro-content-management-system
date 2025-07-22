0017b9eed16a0e60babecc6f362341696f1142fd
krishnadoultani <krishna.k.doultani@gmail.com>
2025-07-22 20:43:25 +0530

    Add .next/static/chunks/src_app_admin_page_tsx_0596e1ea._.js 1753197204381

---
+++ .next/static/chunks/src_app_admin_page_tsx_0596e1ea._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_78ed9d94._.js"
+],
+    source: "dynamic"
+});
